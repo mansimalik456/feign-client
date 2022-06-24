859f0aecbf2241727fa63f03a3afc404bb9f8b95
mansimalik456 <mansi.malik@oodles.io>
2022-06-24 20:15:04 +0530

    feign client commit

---
+++ src/main/java/com/feignClient/client/ScaffoldClient.java
@@ -0,0 +1,16 @@
+package com.feignClient.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(value = "communication-scaffold-service", url="localhost:9008")
+public interface ScaffoldClient {
+	
+//	@GetMapping(UrlConstants.GET_USER)
+	@GetMapping("/v1/management/user/{userId}")
+	public Object getUserById(@PathVariable long userId, @RequestHeader("Authorization") String accessToken);
+
+
+}
---
+++ src/main/java/com/feignClient/controller/ScaffoldController.java
@@ -0,0 +1,27 @@
+package com.feignClient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.feignClient.client.ScaffoldClient;
+
+@RestController
+@RequestMapping("/feignClient")
+public class ScaffoldController {
+	
+	@Autowired ScaffoldClient scaffoldClient;
+	
+	@GetMapping("/v1/management/user/{userId}")
+	public ResponseEntity<Object> getUserById(@PathVariable long userId, @RequestHeader("Authorization") String accessToken) {
+		Object data = scaffoldClient.getUserById(userId, accessToken);
+		return new ResponseEntity<Object>(data, HttpStatus.OK);
+	}
+
+
+}

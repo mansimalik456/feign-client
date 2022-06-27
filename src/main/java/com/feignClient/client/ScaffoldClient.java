71068e6a37b231e85e9646d8fcc309d5e5629858
mansimalik456 <mansi.malik@oodles.io>
2022-06-27 12:58:04 +0530

    feign client commit

--- src/main/java/com/feignClient/client/ScaffoldClient.java
+++ src/main/java/com/feignClient/client/ScaffoldClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "communication-scaffold-service", url="localhost:9008")
+@FeignClient(value = "communication-scaffold-service", url="localhost:8080")
 public interface ScaffoldClient {
 	
 //	@GetMapping(UrlConstants.GET_USER)
